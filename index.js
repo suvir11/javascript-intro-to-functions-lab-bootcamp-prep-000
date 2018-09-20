3bdd3de7be8c5dafd20d16eb373bb2f00d02ad27
suvir@me.com <suvir@me.com>
2018-09-20 22:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string === "I love you, Grandma.") {
-    return("")
+    return("I love you, too.")
   }
   if(string === string.toUpperCase()) {
     return("YES INDEED!");
